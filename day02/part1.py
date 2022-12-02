29943b045a18d59a5119ff61e72f8f4640efbdc2
Brad Glascock <brad.glascock@gmail.com>
2022-12-02 10:14:11 -0600

    day 2 solution

---
+++ day02/part1.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+LOSE = 0
+DRAW = 3
+WIN = 6
+ROCK = 1
+PAPER = 2
+SISSORS = 3
+
+
+def compute(s: str) -> int:
+
+    score = 0
+    lines = s.splitlines()
+    for line in lines:
+        points = 0
+        opponent, mine = line.split(' ')
+
+        match opponent:
+            case 'A':
+                match mine:
+                    case 'X':
+                        points = DRAW + ROCK
+                    case 'Y':
+                        points = WIN + PAPER
+                    case 'Z':
+                        points = LOSE + SISSORS
+
+        match opponent:
+            case 'B':
+                match mine:
+                    case 'X':
+                        points = LOSE + ROCK
+                    case 'Y':
+                        points = DRAW + PAPER
+                    case 'Z':
+                        points = WIN + SISSORS
+
+        match opponent:
+            case 'C':
+                match mine:
+                    case 'X':
+                        points = WIN + ROCK
+                    case 'Y':
+                        points = LOSE + PAPER
+                    case 'Z':
+                        points = DRAW + SISSORS
+
+        score += points
+
+    return score
+
+
+INPUT_S = '''\
+A Y
+B X
+C Z
+'''
+EXPECTED = 15
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
