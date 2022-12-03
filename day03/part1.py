5bd71d7cd932a4f73abf1a8082aa9e206bafda0d
Brad Glascock <brad.glascock@gmail.com>
2022-12-03 12:55:16 -0600

    day 3 solution

---
+++ day03/part1.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+import string
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+
+    alpha = string.ascii_letters
+    total = 0
+    lines = s.splitlines()
+
+    for line in lines:
+        mid = int(len(line)/2)
+        bag1 = line[:mid]
+        bag2 = line[mid:]
+        common = ''.join(set(bag1).intersection(bag2))
+        total += int(alpha.find(common) + 1)
+
+    return total
+
+
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 157
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day03/part2.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+import string
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+
+    alpha = string.ascii_letters
+    total = 0
+    lines = s.splitlines()
+    bag = []
+    bagNum = 0
+
+    for line in lines:
+        bag.append(line)
+        bagNum += 1
+        if bagNum == 3:
+            common = ''.join(set(bag[0]).intersection(bag[1], bag[2]))
+            total += int(alpha.find(common) + 1)
+            bag = []
+            bagNum = 0
+
+    return total
+
+
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 70
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
